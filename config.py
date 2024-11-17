9aaa9097d1f74e41803352a7daedec04e02383f0
Muzamil Hussain <49637191+Hussainflr@users.noreply.github.com>
2024-11-17 19:07:13 +0800

    update

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 
 def load_config(config_file="config.yaml"):
@@ -9,6 +10,8 @@ def load_config(config_file="config.yaml"):
 # Load config.yaml
 config = load_config()
 
+# ALIBABA_CLOUD_ACCESS_KEY_ID = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+# ALIBABA_CLOUD_ACCESS_KEY_SECRET = os.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET ")
 
 ALIBABA_CLOUD_ACCESS_KEY_ID = config['credentials']['ALIBABA_CLOUD_ACCESS_KEY_ID']
 ALIBABA_CLOUD_ACCESS_KEY_SECRET = config['credentials']['ALIBABA_CLOUD_ACCESS_KEY_SECRET']
@@ -19,3 +22,4 @@ OUTPUT_VIDEO_PATH = config['video']['OUTPUT_VIDEO_PATH']
 SIMILARITY_THRESHOLD = config['video']['SIMILARITY_THRESHOLD']
 EMAIL_REG = config['email']['EMAIL_REG']
 CODEC_FORMAT = config['video']['CODEC_FORMAT']
+CONFIDENCE = config["CONFIDENCE"]
